9233c83806a54da0dfeef922f7243f83ac96f16d
Hikmat Ali <hikmat.aliyevm@gmail.com>
2024-05-11 11:31:08 +0200

    Add cheerio library to parse the html page

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,25 +1,47 @@
 "use client";
+import { scrapeAndStoreProduct } from "@/lib/actions";
 import { FormEvent, useState } from "react";
 
 const isValidProductUrl = (url: string) => {
+
   if (!url.startsWith('http://') && !url.startsWith('https://')) {
     url = 'https://' + url; // Prepend 'https://' if scheme is missing
   }
-  console.log(url.toString())
-  const parsedUrl = new URL(url.toString());
-  console.log(parsedUrl)
+
+  try {
+    const parsedUrl = new URL(url);
+    const hostName = parsedUrl.hostname;
+    if(hostName.includes('amazon.com') || hostName.includes('amazon.')){
+      return true;
+    }
+  }catch(error) {
+    console.log(error);
+    return false;
+  }
 
   return false;
 }
 
 const SearchBar = () => {
   const [searchPrompt, setSearchPrompt] = useState('');
-  
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    const isValidLink = isValidProductUrl(searchPrompt);
-    alert(isValidLink ? 'Valid link' : 'Invalid link')
+    // const isValidLink = isValidProductUrl(searchPrompt);
+    // if(!isValidLink) return alert("Please provide a valid Amazon link");
+
+    try {
+      setIsLoading(true);
+
+      //scrape the product
+      const product = await scrapeAndStoreProduct(searchPrompt);
+    } catch (error) {
+      console.log(error)
+    }finally{
+      setIsLoading(false);
+    }
   }
 
   return (
@@ -34,7 +56,12 @@ const SearchBar = () => {
         placeholder="Enter product link" 
         className="searchbar-input"
       />
-      <button type="submit" className="searchbar-btn">Search</button>
+      <button 
+        type="submit"
+        className="searchbar-btn"
+        disabled={searchPrompt === ''}>
+        {isLoading ? 'Searching' : 'Search'}
+      </button>
     </form>
   )
 }
