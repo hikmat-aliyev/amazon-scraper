d0e98621e384d9fd82dd95961bb84ff81b72ef89
Hikmat Ali <hikmat.aliyevm@gmail.com>
2024-05-23 19:44:51 +0200

    Create Email body generator function

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,10 +1,24 @@
 'use client'
 
-import { Description, Dialog, DialogPanel, DialogTitle, Transition } from '@headlessui/react'
-import { useState } from 'react'
+import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react'
+import Image from 'next/image'
+import { Fragment, useState } from 'react'
 
 const Modal = () => {
-  let [isOpen, setIsOpen] = useState(false)
+  let [isOpen, setIsOpen] = useState(false);
+  const [email, setEmail] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    await addUserEmailToProduct(productId, email);
+
+    setIsSubmitting(false)
+    setEmail('')
+    setIsOpen(false)
+  }
 
   return (
     <>
@@ -13,20 +27,62 @@ const Modal = () => {
       </button>
 
 
-      <Transition>
-        <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
-          <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-            <DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
-              <DialogTitle className="font-bold">Deactivate account</DialogTitle>
-              <Description>This will permanently deactivate your account</Description>
-              <p>Are you sure you want to deactivate your account? All of your data will be permanently removed.</p>
-              <div className="flex gap-4">
-                <button onClick={() => setIsOpen(false)}>Cancel</button>
-                <button onClick={() => setIsOpen(false)}>Deactivate</button>
+      <Transition appear show={isOpen} as={Fragment}>
+      <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
+        {/* The backdrop, rendered as a fixed sibling to the panel container */}
+        <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+
+        {/* Full-screen container to center the panel */}
+        <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+          {/* The actual dialog panel  */}
+          <DialogPanel className="max-w-[600px] space-y-4 bg-white p-6 rounded-xl">
+            <div className='flex justify-between'>
+              <div className='border border-gray-300 w-fit p-3 rounded-lg'>
+                <Image
+                  src='/assets/icons/logo.svg'
+                  alt='price tag'
+                  width={26}
+                  height={26}
+                />
               </div>
-            </DialogPanel>
-          </div>
-        </Dialog>
+              <button onClick={() => setIsOpen(false)}>
+                <Image
+                  src='/assets/icons/x-close.svg'
+                  alt='close button'
+                  width={26}
+                  height={26}
+                />
+              </button>
+            </div>
+            <DialogTitle className="text-gray-800 font-semibold text-[1.1rem]">Stay updated with product pricing alerts right in your inbox!</DialogTitle>
+            <p className="text-gray-600 text-[0.9rem]">Never miss a bargain again with our timely alerts!</p>
+            <form action="" className='flex flex-col'>
+              <label htmlFor='email' className='text-gray-700 text-[0.9rem] font-semibold'>Email address</label>
+              <div className="dialog-input_container">
+                <Image 
+                  src="/assets/icons/mail.svg"
+                  alt='mail'
+                  width={18}
+                  height={18}
+                />
+
+                <input 
+                  required
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter your email address"
+                  className='dialog-input'
+                />
+              </div>
+              <button className='dialog-btn'>
+                {isSubmitting ? 'Submitting...' : 'Track'}
+              </button>
+            </form>
+          </DialogPanel>
+        </div>
+      </Dialog>
       </Transition>
     </>
   )
--- components/Trending.tsx
+++ components/Trending.tsx
@@ -9,7 +9,7 @@ const Trending = async () => {
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
 
-      <div className="flex justify-center items-center flex-wrap gap-14 border border-red-600">
+      <div className="flex justify-center items-center flex-wrap gap-14">
       {allProducts?.map((product) => (
         <div key={product._id} className="flex flex-wrap gap-x-8 gap-y-16">
           <ProductCard key={product._id} product={product}/>
