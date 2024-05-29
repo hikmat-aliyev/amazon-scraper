ea5a99e2a40ebe5c98f3d05683933b4b3742ea87
Hikmat Ali <hikmat.aliyevm@gmail.com>
2024-05-29 18:06:20 +0200

    Add headers to to get the same html structure on deployment

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -23,10 +23,17 @@ export async function scrapeAmazonProduct(url:string) {
   }
 
   try {
-    const response = await axios.get(url, options);
-    const $ = cheerio.load(response.data)
+    const response = await axios.get(url, {
+      ...options,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br'
+      }
+    });
     
-    console.log(response.data);
+    const $ = cheerio.load(response.data)
     //Extract product info
     const title = $("#productTitle").text().trim();
     const currentPriceWhole = extractPrice(
